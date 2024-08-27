cd2c3fb4fdda4e0b977a30d6efb07addb8cf904f
hardeep_cleprtech <hardeep@cleprtech.com>
2024-08-27 10:41:29 +0530

    mmm

--- .history/src/lib/rhinoContext_20240826131623.js
+++
@@ -1,9 +0,0 @@
-const rhinoContext = {
-  publicPath: "readabilitytutor_en_wasm_v3_0_0.rhn",
-  customWritePath: "3.0.0_coffee_maker_wasm.rhn",
-};
-
-(function () {
-  if (typeof module !== "undefined" && typeof module.exports !== "undefined")
-    module.exports = rhinoContext;
-})();
--- .history/src/lib/rhinoContext_20240827100212.js
+++
@@ -1,9 +0,0 @@
-const rhinoContext = {
-  publicPath: "contexts/coffee_maker_wasm.rhn",
-  customWritePath: "3.0.0_coffee_maker_wasm.rhn",
-};
-
-(function () {
-  if (typeof module !== "undefined" && typeof module.exports !== "undefined")
-    module.exports = rhinoContext;
-})();
