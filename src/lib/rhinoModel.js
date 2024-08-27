aee67aba45d55de3fe70be2804056efd42aa8b92
hardeep_cleprtech <hardeep@cleprtech.com>
2024-08-27 12:34:20 +0530

    ...all dir pusing

--- src/lib/rhinoModel.js
+++
@@ -1,9 +0,0 @@
-const rhinoModel = {
-  publicPath: "models/rhino_params.pv",
-  customWritePath: "3.0.0_rhino_params.pv",
-};
-
-(function () {
-  if (typeof module !== "undefined" && typeof module.exports !== "undefined")
-    module.exports = rhinoModel;
-})();
